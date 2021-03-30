06a05f4227a40b0dc30de4322c7237f2f1bac421
Viktor Zhukov <v_zhukov1985@mail.ru>
2021-03-30 21:03:43 +0600

    Sync 2

---
+++ Common/Models/VocalType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Models
+{
+    public enum VocalType
+    {
+        Male,
+        Female,
+        Duet
+    }
+}
---
+++ NewSongsProject/Services/SPServerSocket.cs
@@ -0,0 +1,74 @@
+﻿using EzSockets;
+using NewSongsProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace NewSongsProject.Services
+{
+    public class SPServerSocket
+    {
+        private EzSocketListener socket;
+        private List<SPClient> activeClients;
+
+        public delegate void MessageReceivedHandler(EzSocket socket, string header, string data);
+
+        public event MessageReceivedHandler OnMessageReceived;
+
+        public SPServerSocket(int port)
+        {
+            socket = new EzSocketListener(new EzEventsListener()
+            {
+                OnNewConnectionHandler = OnNewConnectionHandler,
+                OnMessageReadHandler = OnSocketMessageReceived,
+                OnConnectionClosedHandler = OnConnectionClosedHandler
+            });
+
+            activeClients = new List<SPClient>();
+
+            socket.ListenAsync(55555);
+        }
+
+        public void SendMessageToClient(EzSocket socket, string header, string data)
+        {
+            if (string.IsNullOrEmpty(header))
+                header = "";
+            if (string.IsNullOrEmpty(data))
+                data = "";
+
+            socket.SendMessageAsync(Encoding.UTF8.GetBytes(header + "|" + data));
+        }
+
+        public void BroadcastMessage(string header, string data)
+        {
+            foreach (var client in activeClients)
+            {
+                client.Socket.SendMessageAsync(Encoding.UTF8.GetBytes(header + "|" + data));
+            }
+        }
+
+        private void OnNewConnectionHandler(EzSocket socket)
+        {
+            socket.StartReadingMessages();
+            activeClients.Add(new SPClient(socket));
+        }
+
+        private void OnConnectionClosedHandler(EzSocket socket)
+        {
+            activeClients.RemoveAll(c => !c.Socket.Connected);
+        }
+
+        private void OnSocketMessageReceived(EzSocket socket, byte[] data)
+        {
+            string msg = Encoding.UTF8.GetString(data);
+            string strHeader = msg.Substring(0, msg.IndexOf('|'));
+            string strData = msg.Substring(msg.IndexOf('|')+1);
+
+            OnMessageReceived?.Invoke(socket, strHeader, strData);
+        }
+    }
+}
