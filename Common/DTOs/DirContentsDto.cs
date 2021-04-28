7b57d12e7778fe7a80116c40b7c7239204fa20b9
Viktor Zhukov <v_zhukov1985@mail.ru>
2021-04-28 20:57:30 +0600

    DirContentsDTo added

---
+++ Common/DTOs/DirContentsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Common.Models;
+
+namespace Common.DTOs
+{
+    public class DirContentsDto
+    {
+        List<TrackListItem> TrackListItems { get; set; }
+        bool IsTopDir { get; set; }
+        string UpDirPath { get; set; }
+    }
+}
